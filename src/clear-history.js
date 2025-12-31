6473959e958b515f8bfa0506606ff71ede15f95a
Richard Lindner <rlindner81@gmail.com>
2025-12-31 10:02:24 +0100

    initial

---
+++ src/clear-history.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const FILTER_URL_PREFIX = "https://www.google.";
+const MILLIS_PER_DAY = 1000 * 60 * 60 * 24;
+
+const clearHistoryItem = async (item) => {
+  if (!item.url.startsWith(FILTER_URL_PREFIX)) {
+    return;
+  }
+  console.log("clearing %s", item.url);
+  return await chrome.history.deleteUrl({ url: item.url });
+};
+
+const clearHistoryItems = async (items) =>
+  await Promise.all(items.map(clearHistoryItem));
+
+chrome.history.onVisited.addListener(clearHistoryItem);
+
+chrome.runtime.onInstalled.addListener(async () => {
+  const oneMonthAgo = new Date().getTime() - 30 * MILLIS_PER_DAY;
+  chrome.history.search(
+    { text: FILTER_URL_PREFIX, startTime: oneMonthAgo, maxResults: 5000 },
+    clearHistoryItems,
+  );
+});
